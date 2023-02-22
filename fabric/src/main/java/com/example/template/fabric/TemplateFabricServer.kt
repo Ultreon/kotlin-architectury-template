c60c118f56d3545cde0dbce9f4231850e5f420da
Qboi123 <qboiwastaken@gmail.com>
2023-02-22 06:13:35 +0100

    Fix server-side issue.

--- fabric/src/main/java/com/example/template/fabric/TemplateFabricServer.kt
+++ fabric/src/main/java/com/example/template/fabric/TemplateFabricServer.kt
@@ -1,11 +1,11 @@
 package com.example.template.fabric
 
-import com.example.template.client.TemplateModClient
+import com.example.template.server.TemplateModServer
 import net.fabricmc.api.DedicatedServerModInitializer
 
 @Suppress("unused")
 object TemplateFabricServer : DedicatedServerModInitializer {
     override fun onInitializeServer() {
-        TemplateModClient.init()
+        TemplateModServer.init()
     }
 }
